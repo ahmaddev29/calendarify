3f1915d51d1c844cafa405d788984ba398260ef8
Muhammad Ahmad <mait124698@gmail.com>
2024-10-15 22:39:34 +0500

    initial commit

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,28 @@
+import Header from "@/app/components/Header";
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "./../globals.css";
+
+const noto = Noto_Sans({ subsets: ["latin"], weight: ["300", "400", "700"] });
+
+export const metadata: Metadata = {
+  title: "Calendarify",
+  description: "An app used to schedule meetings",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={noto.className}>
+        <main className="container">
+          <Header />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
